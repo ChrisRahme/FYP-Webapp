4ac31f8d2310482eb667bcef0c21fbb07a6f242d
Chris Rahmé <chris.rahme@hotmail.com>
2021-04-02 00:58:05 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,15 @@
 /* eslint-disable no-console */
 /* eslint-disable camelcase */
 
-const server = "localhost";
-const rasa_action_endpoint_url = `http://${server}:5055/webhook`
-const rasa_server_url = `http://${server}:5005/webhooks/rest/webhook`;
-// const server = "194.126.17.114";
-// const rasa_action_endpoint_url = `http://${server}/webhook`
-// const rasa_server_url = `http://${server}/webhooks/rest/webhook`;
+// const rasa_action_endpoint_url = "http://localhost:5055/webhook";
+// const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const IP = "194.126.17.114";
+const rasa_action_endpoint_url = `http://${server}/webhook`;
+const rasa_server_url = `http://${server}/webhooks/rest/webhook`;
 
 
 const botPic  = "./assets/img/botAvatar_rasa.png";
-const userPic = "./assets/img/userAvatar.jpg"
+const userPic = "./assets/img/userAvatar.jpg";
 
 const action_name = "action_utter_greet";
 const sender_id = uuidv4();
